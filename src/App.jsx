ad69408cb12d0fec34687a2d8328bc6825925a0d
Tade77 <adekunletaofeeq@mail.com>
2023-04-05 22:50:46 +0100

    setting up Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
-import "./App.css";
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import Store from "./Pages/Store";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="container">
-      <h1>hello</h1>
-    </div>
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
